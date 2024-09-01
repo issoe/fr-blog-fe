d5a6e7b77ae7f88c1184b8f59f1880df8c6f9c94
issoe <twmnno@gmail.com>
2024-09-02 02:02:29 +0700

    init source fe

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+
+import React from 'react';
+import Homepage from './Pages/Homepage';
+import TestAPI from './Pages/TestAPI';
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+      {/* <TestAPI /> */}
+    </div>
+  );
+}
+
+export default App;
