146ee82f8c34a10ad916af96e8e32bbe0fa27d68
issoe <twmnno@gmail.com>
2024-09-02 17:21:11 +0700

    test on render

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 // src/App.js
 
 import React from 'react';
-// import Homepage from './Pages/Homepage';
-import TestAPI from './Pages/TestAPI';
+import Homepage from './Pages/Homepage';
+// import TestAPI from './Pages/TestAPI';
 
 function App() {
   return (
     <div className="App">
-      {/* <Homepage /> */}
-      <TestAPI />
+      <Homepage />
+      {/* <TestAPI /> */}
     </div>
   );
 }
