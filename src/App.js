3571fefbd8fdc55b9f717d13432d5aa432c2c47c
issoe <twmnno@gmail.com>
2024-09-06 21:38:58 +0700

    Update FE

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-// src/App.js
-
 import React from 'react';
-import Homepage from './Pages/Homepage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Homepage from './Pages/HomePage/Homepage';
+import Blog from './Pages/Blogs/Blog'
+
 // import TestAPI from './Pages/TestAPI';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Homepage />
+//       {/* <TestAPI /> */} 
+//     </div>
+//   );
+// }
 
 function App() {
   return (
-    <div className="App">
-      <Homepage />
-      {/* <TestAPI /> */}
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/Blog" element={<Blog />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
 export default App;
